95e66ab6755f427a048f4fc6f30fc44128168ea0
Daniel C L <danielcl@Daniels-iMac.local>
2018-05-30 13:33:53 -0700

    Initial Commit

---
+++ swift-4-startkit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-4-startkit
+//
+//  Created by Daniel C L on 30/05/2018.
+//  Copyright © 2018 Daniel C L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ swift-4-startkitTests/swift_4_startkitTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_4_startkitTests.swift
+//  swift-4-startkitTests
+//
+//  Created by Daniel C L on 30/05/2018.
+//  Copyright © 2018 Daniel C L. All rights reserved.
+//
+
+import XCTest
+@testable import swift_4_startkit
+
+class swift_4_startkitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
